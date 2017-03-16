41471a65eedf24b5c51ce2e4cad9f3534401832d
Tamara Bartlett <tamara.bartlett@stelligent.com>
2017-03-16 09:25:51 -0600

    the file with the brew things

---
+++ installBrewConfig.sh
@@ -0,0 +1,31 @@
+
+brew install zsh
+brew install tmux
+brew install awscli
+brew install python
+brew install nodejs
+brew install npm
+brew install speedtest_cli
+brew install gradle
+
+brew cask install docker
+brew cask install spectacle
+brew cask install toggldesktop
+brew cask install tomighty
+brew cask install google-chrome
+brew cask install sococo
+brew cask install 1password
+brew cask install screenhero
+brew cask install iterm2 
+brew cask install dropbox
+brew cask install sublime-text
+brew cask install slack
+brew cask install evernote 
+brew cask install firefox
+brew cask install spotify
+brew cask install kindle
+brew cask install intellij-idea
+
+# Other Stuff I did
+## Download Feedly
+## Any.Do
